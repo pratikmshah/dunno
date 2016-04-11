089ac7831a708ba82abfe0b56072e8a4653345a4
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-10 19:44:07 -0500

    create function to encapsulated section ids

--- js/cheatsheet.js
+++ js/cheatsheet.js
@@ -1,10 +1,31 @@
 $(function(){
   $('body').on('change', '#selection', function() {
-    var execToolTip = function() {
-      $('[data-toggle="tooltip"]').tooltip();
-      $('<p>TESTING</p>').insertBefore('#css');
+    var selectedValue = $("#selection").val().toLowerCase();
+
+    if (selectedValue === 'jquery') {
+      var appendDivsTo = {
+        '1':['#basics-s', '#hierarchy', '#basic-filters', '#content-filters'],
+        '2':['#visibility-filters', '#attribute', '#child-filters'],
+      }
+
+      setTimeout(function() {
+        wrapDiv(appendDivsTo);
+      }, 1100);
     }
 
-    setTimeout(execToolTip, 3000);
   });
 });
+
+// EXECUTION FUNCTIONS
+
+// bootstrap 3 tooltip has bugs - FIXING REQUIRED
+function execToolTip() {
+  $('[data-toggle="tooltip"]').tooltip();
+}
+
+// append divs by taking a collection of arrays in an object
+function wrapDiv(wrapList) {
+  for (var key in wrapList) {
+    $(wrapList[key].toString()).wrapAll('<div>');
+  }
+}
