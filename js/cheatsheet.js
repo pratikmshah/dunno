9eee2010d260af91f9c5e79d42cbc48e3203f626
Pratik Shah <pratik.t.shah@gmail.com>
2016-04-10 20:53:50 -0500

    append divs to list sets for better formatting

--- js/cheatsheet.js
+++ js/cheatsheet.js
@@ -3,20 +3,15 @@ $(function(){
     var selectedValue = $("#selection").val().toLowerCase();
 
     if (selectedValue === 'jquery') {
-      var appendDivsTo = {
-        '1':['#basics-s', '#hierarchy', '#basic-filters', '#content-filters'],
-        '2':['#visibility-filters', '#attribute', '#child-filters'],
-      }
-
       setTimeout(function() {
-        wrapDiv(appendDivsTo);
-      }, 1100);
+        wrapDiv(returnJqueryDivList());
+      }, 1000);
     }
 
   });
 });
 
-// EXECUTION FUNCTIONS
+//************EXECUTION FUNCTIONS***********
 
 // bootstrap 3 tooltip has bugs - FIXING REQUIRED
 function execToolTip() {
@@ -27,5 +22,25 @@ function execToolTip() {
 function wrapDiv(wrapList) {
   for (var key in wrapList) {
     $(wrapList[key].toString()).wrapAll('<div>');
+  }
+}
+
+function returnJqueryDivList() {
+  return {
+    'selectors1':   ['#basics-s', '#hierarchy', '#basic-filters', '#content-filters'],
+    'selectors2':   ['#visibility-filters', '#attribute', '#child-filters'],
+    'attributes':   ['#attributes', '#css', '#dimensions', '#offset', '#data'],
+    'manipulation': ['#copying', '#dom-insertion-around', '#dom-insertion-inside',
+                     '#dom-insertion-outside', '#dom-removal', '#dom-replacement'],
+    'traversing1':  ['#filtering', '#misc-traversing', '#tree-traversal'],
+    'events1':      ['#browser-events', '#document-loading', '#event-handler-attachment', '#form-events',
+                     '#keyboard-events'],
+    'events2':      ['#mouse-events', '#event-object'],
+    'effects':      ['#basics-e', '#custom', '#fading', '#sliding'],
+    'ajax':         ['#global-ajax-event-handlers', '#helper-functions', '#low-level-interface', '#shorthand-methods'],
+    'core1':        ['#jquery-object', '#utilities'],
+    'core2':        ['#dom-element-methods', '#internals', '#deferred-object'],
+    'core3':        ['#callbacks-object']
+
   }
 }
